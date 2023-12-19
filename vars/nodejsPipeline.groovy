ff2814dd29077b752cf9d04860799ab827a52c0a
Razeen <razeen.bahadoor@standardbank.co.za>
2023-12-19 22:19:24 +0200

    fixed imports

--- vars/nodejsPipeline.groovy
+++ vars/nodejsPipeline.groovy
@@ -7,12 +7,8 @@ def call(String env, String awsRegion="eu-west-1") {
     String template = "nodejs"
     def renderer= new podTemplateRenderer()
     String renderedTemplate = renderer.render(template)
-
     // setup variables
-
     def ecrRegistry = "${Constants.awsAccountIds[env]}.dkr.ecr.${awsRegion}.amazonaws.com"
-
-
 
     podTemplate(  podRetention: never(),
             idleMinutes: 1,
@@ -29,8 +25,9 @@ def call(String env, String awsRegion="eu-west-1") {
                 stage('build') {
                     if (env == "DEV" || env == "SIT") {
                         container('kaniko') {
-
                             withCredentials([usernamePassword(credentialsId: 'bitbucket-token', usernameVariable: 'BITBUCKET_USERNAME', passwordVariable: 'BITBUCKET_PASSWORD')]) {
+                                sh "set+x && ls -a"
+                                sh "pwd"
                                 sh "set +x && /kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --destination=${ecrRegistry} --build-arg USERNAME=\$BITBUCKET_USERNAME --build-arg PASSWORD=\$BITBUCKET_PASSWORD --no-push --tar-path image.tar"
                             }
                         }
