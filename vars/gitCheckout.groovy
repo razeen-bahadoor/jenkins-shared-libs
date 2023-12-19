948c2df56b7117b96763c6925a0aa6defc426280
Razeen <razeen.bahadoor@standardbank.co.za>
2023-12-19 15:49:38 +0200

    fix

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,6 +1,7 @@
-def gitCheckout(String url, String revision) {
+def call(String url, String revision) {
     def workingDir = "${env.WORKSPACE}"
     sh "git clone ${repoUrl} ${workingDir}"
     sh "git checkout ${revision}"
     return workingDir
 }
+
