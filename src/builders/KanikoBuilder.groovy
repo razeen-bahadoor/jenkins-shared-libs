794dfe533269a63bc40085241b999eae568edf7a
Razeen <razeen.bahadoor@standardbank.co.za>
2023-12-20 11:35:50 +0200

    test kaniko builder

--- src/builders/KanikoBuilder.groovy
+++ src/builders/KanikoBuilder.groovy
@@ -2,9 +2,8 @@ package builders
 
 import jenkins.Steps
 
-static void build() {
-    Steps steps = new Steps()
-    steps.echo "hello"
+static void build(context) {
+    context.echo "hello"
     //    "set +x && /kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --destination=${containerRegistry} --build-arg USERNAME=\$BITBUCKET_USERNAME --build-arg PASSWORD=\$BITBUCKET_PASSWORD --no-push --tar-path image.tar"
 
 }
