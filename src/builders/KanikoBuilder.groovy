918071e008255f225264a46a7d15b569c4e33ca4
Razeen <razeen.bahadoor@standardbank.co.za>
2023-12-20 11:05:42 +0200

    test kaniko builder

--- src/builders/KanikoBuilder.groovy
+++ src/builders/KanikoBuilder.groovy
@@ -3,7 +3,7 @@ package builders
 class KanikoBuilder {
 
     static void build() {
-        echo "hello"
+        print 'hello'
        //                                sh "set +x && /kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --destination=${containerRegistry} --build-arg USERNAME=\$BITBUCKET_USERNAME --build-arg PASSWORD=\$BITBUCKET_PASSWORD --no-push --tar-path image.tar"
 
     }
--- vars/Constants.groovy
+++ vars/Constants.groovy
@@ -1,13 +1,12 @@
 class Constants {
+
     public static final Map<String,String> awsAccountIds = [
-            "DEV": "xyz",
-            "SIT": "xyz",
-            "UAT": "xyz",
-            "PROD": "xyz"
+            'DEV': 'xyz',
+            'SIT': 'xyz',
+            'UAT': 'xyz',
+            'PROD': 'xyz'
     ]
 
-
-    public final String awsCrossAccountDeploymentRole = "Bounded-jenkins-crossaccount-deployment-role"
-
+    public final String awsCrossAccountDeploymentRole = 'Bounded-jenkins-crossaccount-deployment-role'
 
 }
