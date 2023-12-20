182acacb4d1d2dc6d95c9727dceeace6521f022c
Razeen <razeen.bahadoor@standardbank.co.za>
2023-12-20 13:18:53 +0200

    added app name

--- src/pipelinestep/build/KanikoBuilder.groovy
+++ src/pipelinestep/build/KanikoBuilder.groovy
@@ -19,9 +19,7 @@ class KanikoBuilder extends Step {
     // context.sh "/kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --destination=${destination} --build-arg USERNAME=${bitbucketUsername} --build-arg PASSWORD=${bitbucketPassword} --no-push --tar-path image.tar"
     // }
 
-    void build(
-        Boolean noPush = true,String destination,
-        String bitbucketUsername, String bitbucketPassword) {
+    void build() {
         steps.echo getScript()
         this.steps.sh "/kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --destination=${destination} --build-arg USERNAME=${bitbucketUsername} --build-arg PASSWORD=${bitbucketPassword} --no-push --tar-path image.tar"
     }
