31969b71a51358a7497d08da500e8cf54673f209
Razeen <razeen.bahadoor@standardbank.co.za>
2023-12-20 11:01:21 +0200

    test kaniko builder

--- src/builders/KanikoBuilder.groovy
+++ src/builders/KanikoBuilder.groovy
@@ -4,6 +4,7 @@ class KanikoBuilder {
 
     static void build() {
         sh 'echo test'
+        echo "hello"
        //                                sh "set +x && /kaniko/executor --dockerfile `pwd`/Dockerfile --context `pwd` --destination=${containerRegistry} --build-arg USERNAME=\$BITBUCKET_USERNAME --build-arg PASSWORD=\$BITBUCKET_PASSWORD --no-push --tar-path image.tar"
 
     }
