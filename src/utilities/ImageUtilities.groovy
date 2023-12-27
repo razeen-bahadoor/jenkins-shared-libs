2e273e2aea55988f8c72f50123680acd508d2daa
Razeen <razeen.bahadoor@standardbank.co.za>
2023-12-27 10:20:22 +0200

    added argo build stage

--- src/utilities/ImageUtilities.groovy
+++ src/utilities/ImageUtilities.groovy
@@ -7,7 +7,7 @@ class ImageUtilities {
         return (imageTag ==~ /^[\w\d\.]+\-\d+\-[a-z0-9]{7}\-(release|hotfix|master)$/)
     }
 
-    static String generateImageTag(String version, String buildNumber, String shortCommit, string branchType) {
+    static String generateImageTag(String version, String buildNumber, String shortCommit, String branchType) {
         return String.join("-",version,buildNumber,shortCommit,branchType)
     }
 
