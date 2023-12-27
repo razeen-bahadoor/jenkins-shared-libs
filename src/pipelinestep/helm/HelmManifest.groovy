fe208c6231299ef4fd1db0ffb36151bf11f555cd
Razeen <razeen.bahadoor@standardbank.co.za>
2023-12-27 11:33:25 +0200

    added test values

--- src/pipelinestep/helm/HelmManifest.groovy
+++ src/pipelinestep/helm/HelmManifest.groovy
@@ -9,16 +9,16 @@ class HelmManifest {
             gitClone(steps, helmChartRepoURL)
             steps.dir(config.helmChartRepo) {
                 steps.sh "pwd && ls -a"
-                updateImageTag(steps, config.helmChartValuesPath, config.imageToDeploy, config.env, config.appName)
+                updateImageTag(steps, config.helmChartValuesPath, config.imageToDeploy)
                 steps.sh "git config  user.name 'Jenkins User'"
                 steps.sh "git config  user.email '<>'"
-                stageCommit(steps, "Updates ${appName} image on ${env} to ${imageToDeploy}")
+                stageCommit(steps, "Updates ${cpnfig.appName} image on ${config.env} to ${config.imageToDeploy}")
                 gitPush(steps)
             }
         }
     }
 
-    static void updateImageTag(steps,String valuesFile, String imageToDeploy,String env, String appName) {
+    static void updateImageTag(steps,String valuesFile, String imageToDeploy) {
         steps.sh "sed -ri \'s/^\\s*tag\\s*:.*\$/    tag: \'${imageToDeploy}\'/\' ${valuesFile}/values.yaml"
 
     }  
