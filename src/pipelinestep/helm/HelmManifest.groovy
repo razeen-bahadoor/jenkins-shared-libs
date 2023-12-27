fdbb4b8dc2baf08d2394f1c3c6513a5e237ed58d
Razeen <razeen.bahadoor@standardbank.co.za>
2023-12-27 10:30:09 +0200

    added argo build stage

--- src/pipelinestep/helm/HelmManifest.groovy
+++ src/pipelinestep/helm/HelmManifest.groovy
@@ -11,7 +11,7 @@ class HelmManifest {
     }
 
     static void updateImageTag(steps,String valuesFile, String imageToDeploy,String env, String appName) {
-        steps.sh "sed -ri \'s/^\\s*tag\\s*:.*\$/    tag: \'${imageToDeploy}\'/\' ${filepath}"
+        steps.sh "sed -ri \'s/^\\s*tag\\s*:.*\$/    tag: \'${imageToDeploy}\'/\' ${valuesFile}"
         steps.sh "git config  user.name 'Jenkins User'"
         steps.sh "git config  user.email '<>'"
         stageCommit(steps, "Updates ${appName} image on ${env} to ${imageToDeploy}")
